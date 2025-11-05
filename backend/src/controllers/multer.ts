460d0a0ac086e84c54f1be05532766b8f360e19a
Nahum <flashjzd58@gmail.com>
2025-11-05 17:12:46 -0600

    cambio

--- backend/src/controllers/multer.ts
+++ backend/src/controllers/multer.ts
@@ -1,12 +1,18 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(__dirname, "../../uploads/puntos");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../../uploads/puntos"));
+    cb(null, uploadDir);
   },
   filename: async (req, file, cb) => {
-    const { v4: uuidv4 } = await import('uuid'); // 👈 import dinámico
+    const { v4: uuidv4 } = await import('uuid');
     const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
     cb(null, uniqueName);
   },
