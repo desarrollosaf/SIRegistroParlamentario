0784213f4b0989870b53454ceafd1d3cfd5a662e
cescDG <cesar199403@hotmail.com>
2025-11-18 15:39:58 -0600

    hola

---
+++ src/app/service/agenda.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable, signal, inject, computed } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgendaService {
+
+  private myAppUrl: string;
+  private myAPIUrl: string;
+  private http = inject( HttpClient );
+  constructor() {
+    this.myAppUrl = enviroment.endpoint;
+    this.myAPIUrl ='api/eventos';
+  }
+
+
+  getCatalogos(): Observable<any> {
+    return this.http.get<any>(`${this.myAppUrl}${this.myAPIUrl}/catalogossave/`)
+  }
+  
+}
