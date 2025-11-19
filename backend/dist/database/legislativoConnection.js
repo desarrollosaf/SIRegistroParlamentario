14d2cb3429dec1ef6e97408c4f4ba3b284cf40af
MarSGlez <martin.sanchezg@hotmail.com>
2025-11-19 14:54:45 -0600

    cambios legislativo connection

--- backend/dist/database/legislativoConnection.js
+++ backend/dist/database/legislativoConnection.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelizeCuestionarios = new sequelize_1.Sequelize('congreso_bd', 'congreso_backup', 'qvoMCK4aGaVffBY5Z95g', {
-    host: '160.153.108.105',
+    host: '160.153.180.105',
     dialect: 'mysql',
     define: {
         freezeTableName: true
--- backend/src/database/legislativoConnection.ts
+++ backend/src/database/legislativoConnection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
 const sequelizeCuestionarios = new Sequelize('congreso_bd', 'congreso_backup', 'qvoMCK4aGaVffBY5Z95g', {
-    host: '160.153.108.105',
+    host: '160.153.180.105',
     dialect: 'mysql',
     define: {
         freezeTableName: true 
