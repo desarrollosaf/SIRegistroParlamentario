05b3a90e0400f2570596fd23f0ed434be332b111
Nahum <flashjzd58@gmail.com>
2026-03-06 18:31:10 -0600

    h

--- backend/dist/controllers/agenda.js
+++ backend/dist/controllers/agenda.js
@@ -575,7 +575,6 @@ const catalogos = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                 }
             ]
         });
-      
         const dictamenes = dictamenesRaw.map((p) => {
             var _a, _b;
             const d = p.toJSON();
@@ -586,8 +585,7 @@ const catalogos = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                 id: d.id,
                 punto: `${fecha} - ${(_b = d.evento) === null || _b === void 0 ? void 0 : _b.id} - ${d.punto}`
             };
-        }
-        console.log(dictamenes)
+        });
         const legislatura = yield legislaturas_1.default.findOne({
             order: [["fecha_inicio", "DESC"]],
         });
