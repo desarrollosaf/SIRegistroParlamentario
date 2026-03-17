813263cec0beb9c39496a71b2f28018229628939
cescDG <cesar199403@hotmail.com>
2026-03-17 12:47:58 -0600

    inidecreto

---
+++ src/app/views/pages/iniciativas-decretos/iniciativas-decretos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciativas-decretos',
+  imports: [],
+  templateUrl: './iniciativas-decretos.component.html',
+  styleUrl: './iniciativas-decretos.component.scss'
+})
+export class IniciativasDecretosComponent {
+
+}
