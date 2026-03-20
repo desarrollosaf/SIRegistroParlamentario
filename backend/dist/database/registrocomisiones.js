3276fd5e753b623c5f6e62066ffc4e23eb58c588
MarSGlez <martin.sanchezg@hotmail.com>
2026-03-20 16:41:23 -0600

    cambios conections

--- backend/dist/database/registrocomisiones.js
+++ backend/dist/database/registrocomisiones.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelizeCuestionarios = new sequelize_1.Sequelize('adminplem_siregistroparlamentario', 'usr_siregistro', 'T64X4ZOuiHRCnVWqHVEL', {
-    host: '192.168.36.53',
+    host: '192.168.36.58',
     dialect: 'mysql',
     define: {
         freezeTableName: true
