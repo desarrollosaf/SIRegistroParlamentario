2813a995a608025b6295def843442c93904bf1ca
Nahum <flashjzd58@gmail.com>
2026-03-23 12:10:44 -0600

    cambio

--- backend/dist/controllers/agenda.js
+++ backend/dist/controllers/agenda.js
@@ -4179,7 +4179,7 @@ const enviarWhatsAsistenciaPDF = (req, res) => __awaiter(void 0, void 0, void 0,
         const base64PDF = pdfBuffer.toString('base64');
         const params = {
             token: 'ml56a7d6tn7ha7cc',
-            to: "+525561081154, ",
+            to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741",
             filename: fileName,
             document: base64PDF,
             caption: mensajeTexto
