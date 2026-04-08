96ff966b78a885a5916dd547a378351bfd90df29
MarSGlez <martin.sanchezg@hotmail.com>
2026-04-08 10:56:11 -0600

    cambios agenda

--- backend/dist/controllers/agenda.js
+++ backend/dist/controllers/agenda.js
@@ -2660,12 +2660,7 @@ const enviarWhatsIntervencion = (intervencion) => __awaiter(void 0, void 0, void
         }
         yield axios_1.default.post("https://api.ultramsg.com/instance144598/messages/chat", new URLSearchParams({
             token: "ml56a7d6tn7ha7cc",
-<<<<<<< HEAD
             to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741",
-            //to: "+527222035605",
-=======
-            to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741, +7351799442",
->>>>>>> bd987342d86f8c222358c85234de4b48fe0c586c
             body: `*Intervención destacada ${titulo}*\n*${nombreCompleto}*: ${datos.mensaje}\n`,
             priority: "1",
             referenceId: "",
@@ -2720,12 +2715,7 @@ const enviarWhatsPunto = (req, res) => __awaiter(void 0, void 0, void 0, functio
         const mensaje = `*Punto número ${nopunto}:*\n${puntoTexto}\n\n*Descripción del evento:* ${descripcion}\n*Fecha:* ${fechaFormateada}`;
         const params = {
             token: "ml56a7d6tn7ha7cc",
-<<<<<<< HEAD
             to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741",
-            //to: "+527222035605",
-=======
-            to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741, +7351799442",
->>>>>>> bd987342d86f8c222358c85234de4b48fe0c586c
             body: mensaje,
             priority: "1",
             referenceId: "",
@@ -5551,13 +5541,8 @@ const enviarNotInicioEvento = (req, res) => __awaiter(void 0, void 0, void 0, fu
         const base64PDF = pdfBuffer.toString('base64');
         const params = {
             token: 'ml56a7d6tn7ha7cc',
-<<<<<<< HEAD
-            //to: "+527222035605,",
-            to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741",
-=======
             to: "+527222035605,",
-            // to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741, +7351799442",
->>>>>>> bd987342d86f8c222358c85234de4b48fe0c586c
+            // to: "+527222035605, +527224986377, +527151605569, +527222285798, +527226303741",
             filename: fileName,
             document: base64PDF,
             caption: mensajeTexto
