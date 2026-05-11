fce9ce8ff256a49a91b838557e0b365be7808c2f
MarSGlez <martin.sanchezg@hotmail.com>
2026-05-11 11:28:06 -0600

    cambios connections legislativoeConnections

--- backend/src/database/legislativoConnection.ts
+++ backend/src/database/legislativoConnection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
 const sequelizeCuestionarios = new Sequelize('adminplem_congresoedomex', 'usr_siregistro', 'T64X4ZOuiHRCnVWqHVEL', {
-    host: '192.168.36.58',
+    host: '192.168.36.53',
     dialect: 'mysql',
     define: {
         freezeTableName: true 
